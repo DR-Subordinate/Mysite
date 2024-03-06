efae1d4c03888fdaa1203152dcd6f4f1b72bfb52
DR Subordinate <karin.pspink@gmail.com>
2024-03-07 01:20:00 +0900

    テスト環境用の設定ファイルを作成

---
+++ mysite/settings/config_test.py
@@ -0,0 +1,28 @@
+import os
+
+from .base import *
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["test.***.com"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ["DATABASE_HOST"],
+        "PORT": os.environ["DATABASE_PORT"],
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USERNAME"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "TEST": {
+            "NAME": os.environ["DATABASE_NAME_UNITTEST"],
+        },
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
+}
+
+STATIC_ROOT = BASE_DIR / "static/"
