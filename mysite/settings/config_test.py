f7451009d544aa4c4e49dc99b4b9d069f0010c2e
DR Subordinate <karin.pspink@gmail.com>
2024-03-09 23:13:04 +0900

    config_testのDATABASESの設定をconfig_devと同じに

--- mysite/settings/config_test.py
+++ mysite/settings/config_test.py
@@ -8,21 +8,28 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["test.across-shop.com"]
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "HOST": os.environ["DATABASE_HOST"],
+#         "PORT": os.environ["DATABASE_PORT"],
+#         "NAME": os.environ["DATABASE_NAME"],
+#         "USER": os.environ["DATABASE_USERNAME"],
+#         "PASSWORD": os.environ["DATABASE_PASSWORD"],
+#         "TEST": {
+#             "NAME": os.environ["DATABASE_NAME_UNITTEST"],
+#         },
+#         "OPTIONS": {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     },
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "HOST": os.environ["DATABASE_HOST"],
-        "PORT": os.environ["DATABASE_PORT"],
-        "NAME": os.environ["DATABASE_NAME"],
-        "USER": os.environ["DATABASE_USERNAME"],
-        "PASSWORD": os.environ["DATABASE_PASSWORD"],
-        "TEST": {
-            "NAME": os.environ["DATABASE_NAME_UNITTEST"],
-        },
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 STATIC_ROOT = BASE_DIR / "static/"
