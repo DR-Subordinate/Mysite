5962f15af12d65fa406223354d4f803a8fb765e4
DR Subordinate <karin.pspink@gmail.com>
2024-03-15 13:12:32 +0900

    DATABASESの設定をconfig_devと同じに

--- mysite/settings/config_prod.py
+++ mysite/settings/config_prod.py
@@ -8,18 +8,25 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["marketdatabase.across-shop.com"]
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "HOST": os.environ["DATABASE_HOST"],
+#         "PORT": os.environ["DATABASE_PORT"],
+#         "NAME": os.environ["DATABASE_NAME"],
+#         "USER": os.environ["DATABASE_USERNAME"],
+#         "PASSWORD": os.environ["DATABASE_PASSWORD"],
+#         "OPTIONS": {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     },
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "HOST": os.environ["DATABASE_HOST"],
-        "PORT": os.environ["DATABASE_PORT"],
-        "NAME": os.environ["DATABASE_NAME"],
-        "USER": os.environ["DATABASE_USERNAME"],
-        "PASSWORD": os.environ["DATABASE_PASSWORD"],
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 STATIC_ROOT = BASE_DIR / "static/"
